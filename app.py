80ecc74a1ce54c51beee263138252f850db3a423
Leonie <leonie.frassint@gmail.com>
2026-02-11 16:32:00 +0100

    Premier envoi Flask avec carte et galerie en construction

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask
+import os  # Ajouté : pour lire tes dossiers
+from flask import Flask, render_template, request  # Ajouté : request pour la liste déroulante
 
 app = Flask(__name__)
 
+# Le chemin vers tes images
+CHEMIN_IMAGES = 'static/uploads'
+
 @app.route('/')
 def home():
-    return "Bienvenue sur ma page d'accueil !"
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+@app.route("/map")
+def afficher_ma_carte():
+    return render_template("map.html")
+
+@app.route("/repertoire")
+def repertoire():
+    return render_template("repertoire.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
